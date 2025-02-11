26a72701e97b0df3c8526445b3b601b53d25e4fc
nafiuibnemazhar <159128338+nafiuibnemazhar@users.noreply.github.com>
2025-02-11 12:59:58 +0600

    functionality added

--- Simple Calculator/calculator.php
+++ Simple Calculator/calculator.php
@@ -27,6 +27,28 @@
             $operation = $_POST['operation'];
             $result    = null;
 
+            switch ($operation) {
+                case 'add':
+                    $result = $num1 + $num2;
+                    break;
+                case 'subtract':
+                    $result = $num1 - $num2;
+                    break;
+                case 'multiply':
+                    $result = $num1 * $num2;
+                    break;
+                case 'divide':
+                    if ($num2 != 0) {
+                        $result = $num1 / $num2;
+                    } else {
+                        $result = "Cannot divide by ZERO!";
+                    }
+                    break;
+                default:
+                    $result = "Invalid operation!";
+                    break;
+            }
+            echo "<div class='result'><strong>Result:</strong>$result</div>";
         }
     ?>
 </div>
